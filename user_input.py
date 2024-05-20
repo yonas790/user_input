273e57bae003e12907b9230960a9abe037b8393f
yonas790 <70878858+yonas790@users.noreply.github.com>
2024-05-20 21:18:13 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+age = input("How old are you?")
+location = input("whre did you came from?")
+print("Name : " + name)
+print("age :" + age)
+print("location : " + location)
+
+print("Hello " + name + " You are " + age + " years old and live in " + location)
